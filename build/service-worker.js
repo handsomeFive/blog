c2e9e445989c9160fcdd11003c93ea52cda4bdcb
Grig <1020342566@qq.com>
2019-10-09 21:32:12 +0800

    test

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a6f9969ee2e6d3cc75ce6729d5fb9a0c.js"
+  "./precache-manifest.36bed1e147350d05d19fe76627ac85b9.js"
 );
 
 workbox.clientsClaim();
